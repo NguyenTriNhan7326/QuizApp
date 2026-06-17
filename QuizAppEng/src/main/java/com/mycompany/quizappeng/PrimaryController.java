e770d2cebbe360a86cf0b74baac53dc5dc3a87c4
unknown <gn118790@gmail.com>
2026-06-17 16:16:24 +0700

    init project session

---
+++ QuizAppEng/src/main/java/com/mycompany/quizappeng/PrimaryController.java
@@ -0,0 +1,35 @@
+package com.mycompany.quizappeng;
+
+import com.ntn.utils.MyAlertSingleton;
+import com.ntn.utils.Themes.ThemeTypes;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+
+
+public class PrimaryController implements Initializable {
+    @FXML private ComboBox<ThemeTypes> cbThemes;
+    
+        @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        this.cbThemes.setItems(FXCollections.observableArrayList(ThemeTypes.values()));
+    }
+    
+    public void manageQuestions(ActionEvent e){
+        MyAlertSingleton.getInstance().showMsg("Coming soon...");
+    }
+    
+    public void practice(ActionEvent e){
+        MyAlertSingleton.getInstance().showMsg("Coming soon...");
+    }
+    
+    public void exam(ActionEvent e){
+        MyAlertSingleton.getInstance().showMsg("Coming soon...");
+    }
+
+
+}
---
+++ QuizAppEng/src/main/java/com/ntn/utils/Themes/ThemeTypes.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ntn.utils.Themes;
+
+/**
+ *
+ * @author admin
+ */
+public enum ThemeTypes {
+    DEFAULT,
+    DARK,
+    LIGHT
+    
+}
